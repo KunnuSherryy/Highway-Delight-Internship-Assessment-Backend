89191125624cd9d929530dc6dffb3dff9d3107c2
KunnuSherry <gamingvenom465@gmail.com>
2025-10-31 01:53:06 +0530

    Deployment changes

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const server = http.createServer(app);
 
 // middleware
 app.use(express.json({ limit: '4mb' }));
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+};
+app.use(cors(corsOptions));
 app.use(morgan('dev'));
 
 // routes
